73bae053a026949ef33aae394fd2a850593e026c
Davi161108 <davidavidaguiarfonseca@gmail.com>
2026-05-15 15:39:50 -0300

    Create App.jsx

---
+++ Estojo/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './Componentes/Header'
+import Footer from './Componentes/Footer'
+import PerfilAluno from './Componentes/PerfilAluno'
+
+function App() {
+  return(
+    <div>
+      <Header />
+
+      <PerfilAluno />
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
